66537f24e660a8c5c0e91c7024840d4ecd87c14a
Ahmed Fathi <98648216+ahmedfathy112@users.noreply.github.com>
2024-03-18 23:44:12 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+function calculateAge() {
+      var birthdate = new Date(document.getElementById('birthdate').value);
+      var today = new Date();
+      var age = today.getFullYear() - birthdate.getFullYear();
+      var monthDiff = today.getMonth() - birthdate.getMonth();
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthdate.getDate())) {
+        age--;
+      }
+      document.getElementById('result').innerText ='the age is:'+ age;} 
